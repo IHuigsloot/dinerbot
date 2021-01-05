ef714325b40c2756702fbc6f9c6d4c81d9064e58
Terrence <ter-lou@live.nl>
2021-01-05 17:16:35 +0100

    FEAT: Add product card

--- app/components/card.js
+++ app/components/card.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Card, Avatar } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { Card, Avatar, IconButton, List, Text, Title, Button, useTheme } from 'react-native-paper';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export function RestaurantCard({ title, subtitle, image, navigation, id }) {
@@ -11,13 +11,36 @@ export function RestaurantCard({ title, subtitle, image, navigation, id }) {
         titleStyle={styles.title}
         titleNumberOfLines={2}
         subtitle={subtitle}
-        subtitleStyle={styles.subtitle}
+        subtitleStyle={styles.subtitleRestaurant}
         subtitleNumberOfLines={2}
         left={() => <Avatar.Image style={styles.avatar} source={{ uri: image }} size={50} />}
-        right={() => <MaterialCommunityIcons name="chevron-right" style={{ marginRight: 16 }} color={"black"} size={30} />}
+        right={() => <MaterialCommunityIcons name="chevron-right" style={styles.button} color={"black"} size={30} />}
       />
     </Card>
   );
+}
+
+export function ProductCard({title, subtitle, price, count, onPress}) {
+  const {colors} = useTheme();
+  
+  return (
+    <Card style={styles.card}>
+      <Card.Title
+        title={title}
+        titleNumberOfLines={2}
+        subtitle={"€" + price + "\n" + subtitle}
+        subtitleStyle={styles.subtitleProduct}
+        subtitleNumberOfLines={5}
+        right={() => 
+          count ? ( 
+            <Button style={styles.counterButton} mode="contained" icon="plus" color={colors.accent} onPress={onPress} >{count}</Button>
+          ) : (
+            <IconButton style={[{backgroundColor: colors.accent}, styles.button]} color="white" icon="plus" onPress={onPress} />
+          )
+        }
+      />
+    </Card>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -26,8 +49,12 @@ const styles = StyleSheet.create({
     marginBottom: 6,
   },
 
+  counterButton: {
+    borderRadius: 50,
+    marginRight: 16,
+  },  
+
   button: {
-    backgroundColor: "#009688",
     marginRight: 16,
   },
 
@@ -35,8 +62,12 @@ const styles = StyleSheet.create({
     marginLeft: 8
   },
 
-  subtitle: {
+  subtitleProduct: {
+    marginBottom: 8
+  },
+
+  subtitleRestaurant: {
     marginBottom: 8,
     marginLeft: 8
-  }
+  },
 })
