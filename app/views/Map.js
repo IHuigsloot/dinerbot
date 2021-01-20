2a69ccfdf9c0bb72cdf8ee6a13a722b8f355c3fd
Terrence <ter-lou@live.nl>
2021-01-20 20:17:37 +0100

    WIP: Live updating map

--- app/views/Map.js
+++ app/views/Map.js
@@ -1,15 +1,88 @@
-import React from 'react';
-import { View } from 'react-native';
-import { useTheme } from 'react-native-paper';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Image } from 'react-native';
+import { useTheme, Button } from 'react-native-paper';
+
+import Canvas from 'react-native-canvas';
 
 import Header from '../components/header';
+import { environment } from '../environment/environment';
 
 export default function Map({ navigation }) {
-	const { colors } = useTheme();
+  const [location, setLocation] = React.useState('')
+
+  const canvasRef = useRef(null);
+  let ws
+
+  useEffect(() => {
+    if (!ws) {
+      ws = new WebSocket(`${environment.ws}`);
+    }
+
+    ws.onopen = () => console.log('WebSocket Client Connected');
+    ws.onmessage = (message) => {
+      if (message.data) {
+        const res = JSON.parse(message.data);
+        console.log(res.robot.location)
+        setLocation(res.robot.location);
+      }
+    };
+    ws.onerror = (e) => {
+      // an error occurred
+      console.log(e.message);
+    };
+    return () => {
+      console.log('close connection');
+      ws.close();
+    }
+  }, [])
+
+  React.useEffect(() => {
+    handleCanvas();
+  }, [location])
+  
+  const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+
+  const getMarkerPosition = value => {
+    const charNum = letters.indexOf(value[0]) + 1;
+    const num = value[2] ? value[1] + value[2] : value[1];
+
+    const y = num * 40 - 20;
+    const x = charNum * 40 - 20;
+    return {x, y}
+  }
+
+  const setMarker = (ctx, coordinate) => {
+    const mp = getMarkerPosition(coordinate);
+    ctx.beginPath();
+    ctx.arc(mp.x, mp.y, 9, 0, Math.PI * 2, true);
+    ctx.fill();
+  }
+
+  const setPathing = (ctx, begin, end) => {
+    mp = getMarkerPosition(begin);
+    ctx.moveTo(mp.x, mp.y);
+    mp = getMarkerPosition(end);
+    ctx.lineTo(mp.x, mp.y);
+    ctx.stroke();
+  }
+
+
+  const handleCanvas = () => {
+    const canvas = canvasRef.current;
+    canvas.width = 400
+    canvas.height = 400
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'red';
+    ctx.strokeStyle = 'red';
+    setMarker(ctx, location);
+  }
 
   return (
     <View>
       <Header title="Map" home />
+      <Image source={require('../img/map.jpeg')} style={{marginTop: 12, marginHorizontal: 6, height: 400, width: 400}} ></Image>
+      <Canvas style={{top: 68, left: 6, position: "absolute"}} ref={canvasRef} />
     </View>
   )
 }
