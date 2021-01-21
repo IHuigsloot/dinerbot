969706071a8e1c2586f396f5a92e404384da2111
Terrence <ter-lou@live.nl>
2021-01-21 14:47:18 +0100

    WIP: Live location update

--- app/views/Map.js
+++ app/views/Map.js
@@ -8,21 +8,23 @@ import Header from '../components/header';
 import { environment } from '../environment/environment';
 
 export default function Map({ navigation }) {
-  const [location, setLocation] = React.useState('')
+  const { colors } = useTheme();
+  const [location, setLocation] = useState('')
+  const [pathList, setPathList] = useState([
+    'B8', 'C8', 'C7', 'C6', 'C5', 'C4', 'D4', 'D3', 'D2', 'E2'
+  ])
 
   const canvasRef = useRef(null);
   let ws
 
   useEffect(() => {
-    if (!ws) {
-      ws = new WebSocket(`${environment.ws}`);
-    }
+    !ws ? ws = new WebSocket(`${environment.ws}`) : null ;
 
     ws.onopen = () => console.log('WebSocket Client Connected');
     ws.onmessage = (message) => {
       if (message.data) {
         const res = JSON.parse(message.data);
-        console.log(res.robot.location)
+        console.log(res.robot.location);
         setLocation(res.robot.location);
       }
     };
@@ -58,14 +60,17 @@ export default function Map({ navigation }) {
     ctx.fill();
   }
 
-  const setPathing = (ctx, begin, end) => {
-    mp = getMarkerPosition(begin);
+  const setPathing = (ctx) => {
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = colors.accent;
+    let mp = getMarkerPosition(pathList[0]);
     ctx.moveTo(mp.x, mp.y);
-    mp = getMarkerPosition(end);
-    ctx.lineTo(mp.x, mp.y);
+    pathList.map(item => {
+      mp = getMarkerPosition(item);
+      ctx.lineTo(mp.x, mp.y);
+    })
     ctx.stroke();
   }
-
 
   const handleCanvas = () => {
     const canvas = canvasRef.current;
@@ -75,6 +80,7 @@ export default function Map({ navigation }) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'red';
     ctx.strokeStyle = 'red';
+    setPathing(ctx);
     setMarker(ctx, location);
   }
 
